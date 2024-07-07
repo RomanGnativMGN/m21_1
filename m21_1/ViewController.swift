d7397752a7ccaef7d693e7e78004ee9991732513
Roman Gnativ <gnativromaneducation@gmail.com>
2024-07-07 16:19:23 +0500

    new

---
+++ m21_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m21_1
+//
+//  Created by Гнатив Роман on 07.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
